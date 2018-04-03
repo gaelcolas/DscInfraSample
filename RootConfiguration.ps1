fe62c6bf40cb17271b77442b2a443befe3d29002
gaelcolas <gaelcolas@users.noreply.github.com>
2018-04-03 23:05:36 +0100

    pulling changes from workshop, adding merge demos

--- RootConfiguration.ps1
+++ RootConfiguration.ps1
@@ -1,24 +1,58 @@
-Write-Warning "---------->> Starting Configuration"
-$BuildVersion = $Env:BuildVersion
+$Error.Clear()
+if($Env:BuildVersion) {$BuildVersion = $Env:BuildVersion}
+elseif($gitshortid = (& git rev-parse --short HEAD)) {$BuildVersion = $gitshortid}
+else { $BuildVersion = '0.0.0' }
+$goodPSModulePath = $Env:PSModulePath
 
 configuration "RootConfiguration"
 {
     Import-DscResource -ModuleName PSDesiredStateConfiguration
-
-    #That Module is a configuration, should be defined in Configuration.psd1
     Import-DscResource -ModuleName SharedDscConfig -ModuleVersion 0.0.4
-    Import-DscResource -ModuleName Chocolatey -ModuleVersion 0.0.48
+
 
     $module = Get-Module PSDesiredStateConfiguration
-    $null = & $module {param($tag) Set-PSTopConfigurationName "MOF_$($tag)"} "$BuildVersion"
+    $null = & $module {param($tag,$Env) Set-PSTopConfigurationName "MOF_$($Env)_$($tag)"} "$BuildVersion",$Environment
 
     node $ConfigurationData.AllNodes.NodeName {
-        $(Write-Warning "Processing Node $($Node.Name) : $($Node.nodeName)")
+        Write-Host "`r`n$('-'*75)`r`n$($Node.Name) : $($Node.NodeName) : $(&$module { Get-PSTopConfigurationName })" -ForegroundColor Yellow
+        $Env:PSModulePath = $goodPSModulePath
         (Lookup 'Configurations').Foreach{
             $ConfigurationName = $_
-            $(Write-Warning "`tLooking up params for $ConfigurationName")
+            $(Write-Debug "`tLooking up params for $ConfigurationName")
             $Properties = $(lookup $ConfigurationName -DefaultValue @{})
+            $DscError = [System.Collections.ArrayList]::new()
             Get-DscSplattedResource -ResourceName $ConfigurationName -ExecutionName $ConfigurationName -Properties $Properties
+            $(
+                if($Error[0] -and $LastError -ne $Error[0]) {
+                    $LastIndex = [Math]::Max( ($Error.LastIndexOf($LastError) -1), -1)
+                    if($LastIndex -gt 0) {
+                        $Error[0..$lastIndex].Foreach{
+                            if($Message = Get-DscErrorMessage -Exception $_) {
+                                $null = $DscError.Add($Message)
+                            }
+                        }
+                    }
+                    else {
+                        if($Message = Get-DscErrorMessage -Exception $Error[0]) {
+                            $null = $DscError.add($Message)
+                        }
+                    }
+                    $lastError = $Error[0]
+                }
+
+                if($DscError.count -gt 0) {
+                    $FailMessage = "    $($Node.Name) : $($Node.Role) ::> $_ "
+                    Write-Host -ForeGroundColor Red ($FailMessage + '.' * (55 - $FailMessage.Length) + 'FAILED')
+                    $DscError.Foreach{
+                        Write-Host -ForeGroundColor Yellow "`t$Message"
+                    }
+                }
+                else {
+                    $OkMessage = "    $($Node.Name) : $($Node.Role) ::> $_ "
+                    Write-Host -ForeGroundColor Green ($OkMessage + '.' * (55 -$OkMessage.Length) + 'OK')
+                }
+                $LastCount = $Error.Count
+            )
         }
     }
 }
